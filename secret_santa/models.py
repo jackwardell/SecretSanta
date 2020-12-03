939493516cafacd5117dd1d385b598bb6dff85ac
jackwardell <jack@wardell.xyz>
2020-12-03 21:03:40 +0000

    working secret santa with exclude

--- secret_santa/models.py
+++ secret_santa/models.py
@@ -1,15 +1,20 @@
 import random
 
 import attr
+from functools import wraps
 
 
-@attr.s
+@attr.s(frozen=True)
 class Participant:
     """model for a secret santa participant"""
 
     name = attr.ib()
-    excluded = attr.ib(factory=list)
+    excluded = attr.ib(factory=list, repr=False, hash=False)
+
     # email_address = attr.ib()
+
+    def __str__(self):
+        return self.name
 
 
 @attr.s
@@ -20,32 +25,59 @@ class Result:
     receiver = attr.ib()
 
 
+def retry(num_times):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            iterations = 0
+            while iterations <= num_times:
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    iterations += 1
+
+        return wrapper
+
+    return decorator
+
+
 @attr.s
 class Hat:
     """model for a hat full of participants"""
 
     participants = attr.ib()
 
+    @participants.validator
+    def check_participants(self, attribute, value):
+        if len(self.participants) != len(set([participant.name for participant in value])):
+            raise ValueError(f"{attribute} names MUST be unique!")
+        for v in value:
+            if not [p for p in value if p.name != v.name and p.name not in v.excluded]:
+                raise ValueError(f"{v.name} has NO choices!")
+
+    def choices(self):
+        choices = {}
+        for participant in self.participants:
+            choices[participant] = [
+                p
+                for p in self.participants
+                if p.name != participant.name and p.name not in participant.excluded
+            ]
+        return choices
+
+    @retry(100)
     def distribute(self):
         givers = self.participants.copy()
-        # receivers = self.participants.copy()
-
         random.shuffle(givers)
-        # random.shuffle(receivers)
 
-        results = []
         received = []
+        results = []
 
         for giver in givers:
-            choices = [
-                receiver
-                for receiver in self.participants
-                if receiver != giver
-                and receiver not in received
-                # and receiver not in giver.excluded
-            ]
+            choices = [p for p in self.choices()[giver] if p.name not in received]
             receiver = random.choice(choices)
-            received.append(receiver)
+            received.append(receiver.name)
+
             result = Result(giver, receiver)
             results.append(result)
 
@@ -54,16 +86,26 @@ class Hat:
 
 def test():
     participants = [
-        Participant("jack"),
-        Participant("james"),
-        Participant("jane"),
-        Participant("jill"),
-        Participant("jeremy"),
+        Participant("jack", excluded=["emma"]),
+        Participant("emma", excluded=["jack"]),
+        Participant("alex"),
+        Participant("zoe", excluded=["paddy"]),
+        Participant("paddy", excluded=["zoe"]),
+        Participant("fin"),
+        Participant("giulia", excluded=["ludo"]),
+        Participant("ludo", excluded=["giulia"]),
     ]
 
     hat = Hat(participants)
 
+    hat.choices()
+
     results = hat.distribute()
+
+    for result in results:
+        assert result.giver != result.receiver
+        assert result.giver.name not in result.receiver.excluded
+        assert result.receiver.name not in result.giver.excluded
     print(results)
 
 
