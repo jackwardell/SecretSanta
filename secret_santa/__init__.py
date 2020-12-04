51d6cc77d9e7e1fd3591e922e53fdd786489ce9c
jackwardell <jack@wardell.xyz>
2020-12-04 09:12:43 +0000

    flask app v1

---
+++ secret_santa/__init__.py
@@ -0,0 +1,5 @@
+from .models import Participant
+from .models import Hat
+from .models import Result
+
+__all__ = ["Participant", "Hat", "Result"]
--- secret_santa/models.py
+++ secret_santa/models.py
@@ -81,35 +81,12 @@ class Hat:
             result = Result(giver, receiver)
             results.append(result)
 
+        for result in results:
+            assert result.giver != result.receiver
+            assert result.giver.name not in result.receiver.excluded
+            assert result.receiver.name not in result.giver.excluded
+
         return results
 
 
-def test():
-    participants = [
-        Participant("jack", excluded=["emma"]),
-        Participant("emma", excluded=["jack"]),
-        Participant("alex"),
-        Participant("zoe", excluded=["paddy"]),
-        Participant("paddy", excluded=["zoe"]),
-        Participant("fin"),
-        Participant("giulia", excluded=["ludo"]),
-        Participant("ludo", excluded=["giulia"]),
-    ]
 
-    hat = Hat(participants)
-
-    hat.choices()
-    for i, j in hat.choices().items():
-        print(i,"=",j)
-    print(hat.choices())
-
-    results = hat.distribute()
-
-    for result in results:
-        assert result.giver != result.receiver
-        assert result.giver.name not in result.receiver.excluded
-        assert result.receiver.name not in result.giver.excluded
-    print(results)
-
-
-test()
